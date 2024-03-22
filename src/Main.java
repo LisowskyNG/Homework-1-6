ecb1983ea9ea3a0718ee7576fd0e249647e25806
LisowskyNG <lisowsky@yandex.ru>
2024-03-22 18:46:20 +0100

    Task 1-3

---
+++ src/Main.java
@@ -0,0 +1,73 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        // Пишем код для задачи 1
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println();
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        // Пишем код для задачи 2
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println();
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        // Пишем код для задачи 3
+        for (int i = 0; i <= 17; i++) {
+            if (i %2 == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        // Пишем код для задачи 4
+
+    }
+
+    public static void task5 () {
+        System.out.println("Задача 5");
+        // Пишем код для задачи 5
+
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        // Пишем код для задачи 6
+
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        // Пишем код для задачи 7
+
+    }
+    public static void task8 () {
+        System.out.println("Задача 8");
+        // Пишем код для задачи 8
+
+    }
+
+}
